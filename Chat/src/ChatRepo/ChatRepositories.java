788fcfa651a6a7f58e141f99675c6d5b5a25b75c
ElJeilany MohamedBouya <jeilany2007@gmail.com>
2017-12-22 11:15:56 +0100

    generate web.xml and add to lib gson.jar and jersey.jar

---
+++ Chat/src/ChatRepo/ChatRepositories.java
@@ -0,0 +1,15 @@
+package ChatRepo;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+@Path("/chatrepo")
+@Produces("application/json")
+public class ChatRepositories {
+
+
+}
